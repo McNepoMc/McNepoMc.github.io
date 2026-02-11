49e2f6ac9953e357748b1e88e276f259c5040171
McNepoMc <42786164+McNepoMc@users.noreply.github.com>
2026-02-11 18:17:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,10 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            text: "Magst du mich?",                                    // First interaction
+            yesBtn: "Ja",                                             // Text for "Yes" button
+            noBtn: "Nein",                                               // Text for "No" button
+            secretAnswer: "Ich mag dich nicht, Ich liebe dich! ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
@@ -49,7 +49,7 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        title: "Yayy! I'm the luckiest person in the world! 🎉💝💖💝💓",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
@@ -70,7 +70,7 @@ const CONFIG = {
         floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.3         // Size of heart explosion effect (1.2-2.0 recommended)
+        heartExplosionSize: 1.5        // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
